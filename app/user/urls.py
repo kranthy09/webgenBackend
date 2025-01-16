3a6fc44797330568ce17b89747b4d6dac35fb855
kranthu <g.kranthi2507@gmail.com>
2025-01-17 04:32:00 +0530

    user api

---
+++ app/user/urls.py
@@ -0,0 +1,12 @@
+"""
+urls for user API.
+"""
+
+from user import views
+from django.urls import path
+
+app_name = "user"
+
+urlpatterns = [
+    path("create/", views.CreateUserView.as_view(), name="create"),
+]
